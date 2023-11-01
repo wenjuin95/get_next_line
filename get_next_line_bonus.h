7ec6bd05657a4bd98c58be72227697b540ce2397
Welow <welow@student.42kl.edu.my>
2023-11-01 22:12:17 +0800

    done

---
+++ get_next_line_bonus.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: welow <welow@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/01 20:39:32 by welow             #+#    #+#             */
+/*   Updated: 2023/11/01 20:40:05 by welow            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+/*utils*/
+int		ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+/*get_next_line*/
+char	*get_next_line(int fd);
+char	*create_line(int fd, char *line);
+char	*get_only_next_line(char *line);
+char	*new_line(char *line);
+
+#endif
